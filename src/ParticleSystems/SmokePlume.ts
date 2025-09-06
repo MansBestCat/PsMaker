6416b8bff049c699be9b70fa44180efc1fd5a8f5
Scott Grodberg <scott@mansbestcat.com>
2025-09-06 18:18:37 +0700

    Tweaking values.

--- src/ParticleSystems/SmokePlume.ts
+++ src/ParticleSystems/SmokePlume.ts
@@ -82,28 +82,29 @@ export class SmokePlume extends ParticleSystemBase {
         this.alphaSpline = new LinearSpline((t: number, a: number, b: number) => {
             return a + t * (b - a);
         });
-        this.alphaSpline.addPoint(0.0, 0.7);
+        this.alphaSpline.addPoint(0.0, 0.98);
+        this.alphaSpline.addPoint(0.12, 0.24);
         this.alphaSpline.addPoint(1.0, 0.0);
 
         this.colorSpline = new LinearSplineOut((t: number, a: Color, b: Color, result: Color) => {
             result.copy(a);
             return result.lerp(b, t);
         });
-        this.colorSpline.addPoint(0.0, new Color(0x757575));
-        this.colorSpline.addPoint(1.0, new Color(0x4f4f4f));
+        this.colorSpline.addPoint(0.0, new Color(0xffffff));
+        this.colorSpline.addPoint(1.0, new Color(0xffffff));
 
         this.sizeSpline = new LinearSpline((t: number, a: number, b: number) => {
             return a + t * (b - a);
         });
-        this.sizeSpline.addPoint(0.0, 3.0);
+        this.sizeSpline.addPoint(0.0, 3.4);
         this.sizeSpline.addPoint(1.0, 9.43);
 
         this.velocitySpline = new LinearSpline((t: number, a: number, b: number) => {
             return a + t * (a - b);
         });
-        this.velocitySpline.addPoint(0.0, 4.3);
-        this.velocitySpline.addPoint(0.07, 1.86);
-        this.velocitySpline.addPoint(0.21, 0.71);
+        this.velocitySpline.addPoint(0.0, 9.7);
+        this.velocitySpline.addPoint(0.13, 4.5);
+        this.velocitySpline.addPoint(0.39, 2.25);
         this.velocitySpline.addPoint(1.0, 0.0);
 
         this.emitRateSpline = new LinearSpline((t: number, a: number, b: number) => {
